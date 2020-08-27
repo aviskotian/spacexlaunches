624a80aef99137876a9c3e27c827d904413167b5
Avinash Amin <avinash.amin@globaldelight.com>
2020-08-27 13:53:23 +0530

    Filter UI completed

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -6,9 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filter.component.scss']
 })
 export class FilterComponent implements OnInit {
+  yearArray: any[] = [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020];
 
-  constructor() { }
+  selectedYear: any;
+  selectedLaunch: any;
+  selectedLanding: any;
+  constructor() {
 
+  }
+  yearflterClick(event, newValue) {
+    this.selectedYear = newValue;
+    console.log(this.selectedYear);
+  }
+  launchflterClick(event, newValue) {
+    this.selectedLaunch = newValue;
+    console.log(this.selectedLaunch);
+  }
+  landingflterClick(event, newValue) {
+    this.selectedLanding = newValue;
+    console.log(this.selectedLaunch);
+  }
   ngOnInit() {
   }
 
