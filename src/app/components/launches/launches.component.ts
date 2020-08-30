710144c30d7ed6e655f8170d8fc7c18b647a16c8
Avinash Amin <avinash.amin@globaldelight.com>
2020-08-30 20:39:48 +0530

    Error Handling

--- src/app/components/launches/launches.component.ts
+++ src/app/components/launches/launches.component.ts
@@ -22,6 +22,8 @@ export class LaunchesComponent implements OnInit {
   getData(launchYear, launchSuccess, landSuccess) {
     this.loader = true;
     this.loaded = false;
+    this.errorMsg = false;
+    this.errorCode = false;
     this.dataservice.getData(launchYear, launchSuccess, landSuccess)
     .subscribe((response: any) => {
       this.loaded = true;
